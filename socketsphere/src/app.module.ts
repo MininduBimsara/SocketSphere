cf20df2ee554cfbac646f333eacc10345df1bc4a
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-09-19 00:29:07 +0530

    Created Initial files

--- socketsphere/src/app.module.ts
+++ socketsphere/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule, MessageModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
